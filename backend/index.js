40e8b6690ae98df92e1fe71cc1c41811e1e54cac
Tamal Chakraborty <ckbtamaldipnew@gmail.com>
2024-01-24 10:20:21 +0600

    2nd step done. Add database model and schema

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const { default: mongoose } = require('mongoose')
 const app = express()
 const port = 3000
 
+//* mongo connection 
+async function main() {
+    await mongoose.connect("mongodb+srv://ckbtamaldipnew:v0wZ5g8j5Ee6LyEN@cluster0.pkptg4q.mongodb.net/");
+    console.log("database connected");
+}
+
+//error handling 
+main().catch(err => console.log(err));
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
