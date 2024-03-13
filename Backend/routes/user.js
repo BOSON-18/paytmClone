8d4782f76e7a4c663bcab83d2a72278bd0f98b98
Tamal <ckbtamaldipnew@gmail.com>
2024-03-13 10:05:17 +0600

    complete step 8. included name, password updation api and searching with bulk api

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -3,6 +3,7 @@ const zod = require('zod');
 const { User } = require("../db");
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require("../config");
+const { authmiddleware } = require("../middleware");
 
 const router = express.Router();
 
@@ -92,6 +93,64 @@ router.post("/signin", async (req, res) => {
     //? Other error case handling 
     res.status(411).json({
         message: "Error while logging in"
+    })
+
+})
+
+//*handle update of user name -> they can update either of their there password , firstName or lastName
+
+//? optional schema for choosing either one, if choose one then it should match the schema
+const updateSchema = zod.object({
+    password: zod.string().optional(),
+    firstName: zod.string().optional(),
+    lastName: zod.string().optional(),
+});
+
+router.put('/', authmiddleware, async (req, res) => {
+    //? checking schema for request body
+    const { success } = updateSchema.safeParse(req.body);
+    if (!success) {
+        res.status(411).json({
+            message: "Error while updating information"
+        })
+    }
+
+    //? update anything that in the request body
+    await User.updateOne({ _id: req.userId }, req.body);
+
+    res.json({
+        message: "Update successfully"
+    })
+})
+
+//* handle searching for other users -> will get a query parameter from the user
+
+router.get("/bulk", async (req, res) => {
+
+    //? getting the query given by user
+    const filter = req.query.filter || "";
+
+    //? finding other users that have the filter string in their firstName "or" lastName. users gonna contain one or more than one users
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+
+    //? getting all the users from the user and sending all of their information one by id mapping
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id,
+        }))
     })
 
 })
